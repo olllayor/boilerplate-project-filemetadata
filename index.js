970823424e8b1cbe58ffcbcfcc6424a4a46a4cb3
Olloyor Makhammatnabiyev <o.maxamatnabiyev@polito.uz>
2025-03-17 02:46:59 +0000

    feat: implement file upload API with multer

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({ dest: "uploads/" }); // Files will be saved in the "uploads/" directory
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +14,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  console.log(req.file);
+  
+
+  // Respond with the file metadata
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 
